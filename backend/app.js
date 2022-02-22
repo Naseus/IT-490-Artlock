69eec6923d788ee23f5c8d96bcfcd712301485ad
Thomas DeVries <tdevries4i@gmial.com>
2022-02-22 17:50:25 -0500

    feat: cleaned up backend server

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,12 @@
-const amqp = require('amqplib/callback_api');
+const Server = require('./rabbitMQServer');
 const fs = require('fs');
 
 // Init data for the rabbitMQ connection
 // This can be done in sync since all other server functions require this data
 let mqData = JSON.parse(fs.readFileSync('rabbitMQ.json'));
 
-let mqUrl = (`amqp://${mqData["USER"]}`
-            +`:${mqData["PASSWORD"]}`
-            + `@${mqData["BROKER_HOST"]}`
-            + `:${mqData["BROKER_PORT"]}`
-            + `/${mqData["VHOST"]}`);
+console.log(Server);
 
-        let queue = mqData['QUEUE']
+let server = new Server(mqData);
 
-amqp.connect(mqUrl, (error0, connection) => {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel((error1, channel) => {
-        if (error1) {
-            throw error1;
-        }
-
-        channel.assertExchange(mqData['EXCHANGE']);
-        channel.assertQueue(queue);
-
-        console.log(`Waiting for messages at ${mqUrl} in ${queue}`);
-
-        channel.consume(queue, function(msg) {
-            console.log(`Received ${msg.content.toString()}"`);
-
-            // Formating the routing key to match the php client
-            let replyTo = msg.fields.routingKey + '.response';
-
-            if(replyTo) {
-                channel.publish(mqData["EXCHANGE"], replyTo, msg.content, {
-                    "correlationId":msg.properties.correlationId
-                });
-            }
-            channel.ack(msg);
-        });
-    });
-});
+server.run()
