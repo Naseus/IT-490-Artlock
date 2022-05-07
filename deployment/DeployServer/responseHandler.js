0b69b42c75768fa84b091535074dbc883f986647
Thomas DeVries <tdevries4i@gmail.com>
2022-05-07 15:47:57 -0400

    feat: actions on a deploy

--- deployment/DeployServer/responseHandler.js
+++ deployment/DeployServer/responseHandler.js
@@ -1,15 +1,16 @@
 const  fs = require('fs');
 const path = require('path');
+const { exec } = require('child_process');
 
 class ResponseHandler {
     path = '/home/tdevries4i/';
     ensureDirectoryExistence(filePath) {
-    	let dirname = path.dirname(filePath);
-      	if (fs.existsSync(this.path + dirname)) {
-    		return true;
-  	}
- 	this.ensureDirectoryExistence(dirname);
-  	fs.mkdirSync(this.path + dirname);
+        let dirname = path.dirname(filePath);
+        if (fs.existsSync(this.path + dirname)) {
+            return true;
+        }
+        this.ensureDirectoryExistence(dirname);
+        fs.mkdirSync(this.path + dirname);
     }
 
     error(err, msg) {
@@ -24,21 +25,29 @@ class ResponseHandler {
         } catch(e){
             return this.error(500, e);
         }
-	 try {
+        try {
              fs.rmdirSync(path + obj.pkg_type, {recursive:true,});
-	 } catch(e){console.log(e);}
+        } catch(e){console.log(e);}
         for(let [file, content] of Object.entries(obj.files)){
-	    console.log(`Writing ${file}`);
-    	    this.ensureDirectoryExistence(file);
+            console.log(`Writing ${file}`);
+            this.ensureDirectoryExistence(file);
             fs.open(this.path + file,'w', (err, f)=>{
                     if(err) {
                         console.log(`Failed to write ${file}: ${err}`);
-			return;
+                        return;
                     }
                 fs.writeSync(f, Buffer.from(content.data).toString());
             });
         }
-	console.log('done');
+        let doActions = exec('sh actions.sh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
+        console.log('done');
 
         let rtn = {"status":200, "body":"pkg saved"};
         return Buffer.from(JSON.stringify(rtn));
